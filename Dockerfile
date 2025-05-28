1537aeaef2a48f8aa87e34a3cd6afd0172860c74
John Jolly <jjolly@coreweave.com>
2025-05-28 09:11:31 -0600

    feat(arm64): completed arm64 build process
    fix: add missing ubuntu arm64 sources

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,14 @@ FROM ubuntu:noble
 # Set non-interactive mode for apt
 ENV DEBIAN_FRONTEND=noninteractive
 
+# Annotate apt sources to be amd64 specific
+# This is needed to properly handle arm64 repos
+RUN sed -i 's/^\(URIs:.*\)$/\1\nArchitectures: amd64/' /etc/apt/sources.list.d/ubuntu.sources
+
+COPY ubuntu-arm64.sources /etc/apt/sources.list.d/
+
+RUN dpkg --add-architecture arm64
+
 # Install minimal build dependencies
 RUN apt-get update && apt-get install -y --no-install-recommends \
     build-essential \
@@ -39,6 +47,44 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     python3-dev \
     rsync \
     zstd \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install amd64 build dependencies for userspace tools
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    libbabeltrace-dev \
+    libcap-dev \
+    libnuma-dev \
+    libperl-dev \
+    libpfm4-dev \
+    libslang2-dev \
+    libtraceevent-dev \
+    libunwind-dev \
+    systemtap-sdt-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install arm64 build dependencies for userspace tools
+# To install the python3-dev package, which execs the arm64 python interpreter
+# you must do the following for your contain build environment:
+# * apt-get install -y binfmt-support qemu-user-static
+# * docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    libbabeltrace-dev:arm64 \
+    libbpf-dev:arm64 \
+    libbpfcc-dev:arm64 \
+    libcap-dev:arm64 \
+    libdw-dev:arm64 \
+    libelf-dev:arm64 \
+    libnuma-dev:arm64 \
+    libperl-dev:arm64 \
+    libpfm4-dev:arm64 \
+    libslang2-dev:arm64 \
+    libssl-dev:arm64 \
+    libtraceevent-dev:arm64 \
+    libunwind-dev:arm64 \
+    pkg-config:arm64 \
+    python3-dev:arm64 \
+    python3-setuptools:arm64 \
+    systemtap-sdt-dev:arm64 \
     && rm -rf /var/lib/apt/lists/*
 
 # Set up build environment
--- build-kernel.sh
+++ build-kernel.sh
@@ -14,7 +14,9 @@ if [ "x${ARCH}" == "xarm64" ]; then
   CROSS_COMPILE=aarch64-linux-gnu-
 fi
 
-KMAKE_OPTS="ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE"
+KERNELRELEASE=${KERNELRELEASE:-6.8.0-60.63}
+
+KMAKE_OPTS="ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE KERNELRELEASE=${KERNELRELEASE} -j$(nproc)"
 
 # Set kernel source and version
 KERNEL_SOURCE=${KERNEL_SOURCE:-git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/noble}
@@ -34,7 +36,9 @@ fi
 # Define paths and filenames
 BUILD_DIR="/home/builder/kernel-build"
 SRC_DIR="${KERNEL_TAG}-src"
-SRC_TARBALL="${KERNEL_TAG}-src.tar.xz"
+# Tag may have extra slashes, so keep track of the source parent
+SRC_PARENT_DIR="${SRC_DIR}/.."
+SRC_TARFILE="${KERNEL_TAG}-src.tar.xz"
 
 echo "=== Step 1: Creating kernel build directory ==="
 # Create build directory
@@ -47,18 +51,20 @@ cd "$BUILD_DIR"
 
 # Get kernel source
 echo -e "\n=== Step 2: Preparing kernel source ==="
-if [ -f "/input/${SRC_TARBALL}" ]; then
-  echo "Using provided kernel source tarball"
-  tar xf "/input/${SRC_TARBALL}"
+if [ -f "/input/${SRC_TARFILE}" ]; then
+  echo "Using provided kernel source archive in tar file ${SRC_TARFILE}"
+  tar xf "/input/${SRC_TARFILE}"
 else
+  echo "Source code archive not found at ${SRC_TARFILE}"
   echo "Cloning kernel source from Ubuntu repository (version: $KERNEL_TAG)"
   git -c http.sslVerify=false clone --depth 1 --branch ${KERNEL_TAG} "${KERNEL_SOURCE}" "${BUILD_DIR}/${SRC_DIR}"
 fi
 SRC_TAR_DIR="/output/kernel-source"
 SRC_TAR_DIR_ORIG="${SRC_TAR_DIR}/original"
-echo "Compressing original sources to ${SRC_TAR_DIR_ORIG}"
-mkdir -p "${SRC_TAR_DIR_ORIG}"
-tar cf - ${SRC_DIR} | xz -T0 -9 > "${SRC_TAR_DIR_ORIG}/${SRC_TARBALL}"
+SRC_TAR_FILE_ORIG="${SRC_TAR_DIR_ORIG}/${SRC_TARFILE}"
+echo "Compressing original sources to ${SRC_TAR_FILE_ORIG}"
+mkdir -p $(dirname "${SRC_TAR_FILE_ORIG}")
+tar cf - ${SRC_DIR} | xz -T0 -9 > "${SRC_TAR_FILE_ORIG}"
 
 
 cd "${BUILD_DIR}/${SRC_DIR}"
@@ -86,10 +92,8 @@ CONFIG_SRC="/input/config-${KERNEL_TAG}-${ARCH}"
 if [ -f "${CONFIG_SRC}" ]; then
   echo "Using config file ${CONFIG_SRC}"
   cp "${CONFIG_SRC}" .config
-  echo "Running olddefconfig..."
-  make $KMAKE_OPTS olddefconfig
 else
-  echo "No config provided, building defconfig"
+  echo "No config provided at ${CONFIG_SRC}, building defconfig"
   make $KMAKE_OPTS defconfig
 fi
 
@@ -97,27 +101,95 @@ fi
 # This is a problem with the x86_64 defconfig, and not arm64
 scripts/config -d CONFIG_WERROR
 
-# Create tarball from modified sources with parallel xz compression
-echo -e "\n=== Step 5: Creating source tarball with parallel xz compression ==="
+echo "Checking for certificates..."
+# configure for certificates as provided
+while IFS=' ' read -r config_setting cert_file; do
+  if [ -f "/input/keys/${cert_file}" ]; then
+    echo "Certs found at /input/keys/${cert_file} for ${config_setting} - adding to build"
+    mkdir -p "${BUILD_DIR}/${SRC_DIR}/keys"
+    cp -v "/input/keys/${cert_file}" "${BUILD_DIR}/${SRC_DIR}/keys/"
+    scripts/config --set_str "${config_setting}" "keys/${cert_file}"
+  else
+    echo "Certs not found at /input/keys/${cert_file} - disabling ${config_setting}"
+    scripts/config -d ${config_setting}
+  fi
+done <<< "CONFIG_SYSTEM_TRUSTED_KEYS    trusted-certs.pem
+          CONFIG_SYSTEM_REVOCATION_KEYS revocation-certs.pem"
+
+echo "Running olddefconfig..."
+make $KMAKE_OPTS olddefconfig
+
+# Create tar file from modified sources with parallel xz compression
+echo -e "\n=== Step 5: Creating source tar file with parallel xz compression ==="
 cd "$BUILD_DIR"
 SRC_TAR_DIR_MOD="${SRC_TAR_DIR}/modified"
-mkdir -p "${SRC_TAR_DIR_MOD}"
-echo "Compressing modified sources to ${SRC_TAR_DIR_MOD}"
-tar cf - ${SRC_DIR} | xz -T0 -9 > "${SRC_TAR_DIR_MOD}/${SRC_TARBALL}"
+SRC_TAR_FILE_MOD="${SRC_TAR_DIR_MOD}/${SRC_TARFILE}"
+echo "Compressing modified sources to ${SRC_TAR_FILE_MOD}"
+mkdir -p $(dirname "${SRC_TAR_FILE_MOD}")
+tar cf - ${SRC_DIR} | xz -T0 -9 > "${SRC_TAR_FILE_MOD}"
 cd "${BUILD_DIR}/${SRC_DIR}"
 
 # Build kernel
 echo -e "\n=== Step 6: Building kernel and deb packages ==="
-echo "Building kernel packages with ARCH=$ARCH..."
-time make $KMAKE_OPTS -j$(nproc) deb-pkg
+echo "Building kernel packages with options ${KMAKE_OPTS}"
+time make $KMAKE_OPTS bindeb-pkg
+
+# Build tools
+echo -e "\n=== Step 7: Build linux-tools ==="
+kuname=$(ar p "${BUILD_DIR}/${SRC_PARENT_DIR}/linux-headers"*.deb control.tar.zst | tar x --zstd -O | grep 'Package' | awk '{print $2}' | sed 's/linux-headers-//g' | sed 's/_.*//g')
+kpkg_version=$(ar p "${BUILD_DIR}/${SRC_PARENT_DIR}/linux-headers"*.deb control.tar.zst | tar x --zstd -O | grep 'Version' | awk '{print $2}')
+TOOLS_DIR="${BUILD_DIR}/${SRC_PARENT_DIR}/linux-tools-${kuname}"
+INST_DIR="${TOOLS_DIR}/usr/lib/linux-tools-cw/${kuname}"
+mkdir -p "${INST_DIR}"
+
+pushd "${BUILD_DIR}/${SRC_DIR}/tools/perf"
+make $KMAKE_OPTS NO_LIBBPF=1
+cp perf "${INST_DIR}/"
+popd
+
+pushd "${BUILD_DIR}/${SRC_DIR}/tools/bpf/bpftool"
+make $KMAKE_OPTS
+cp bpftool "${INST_DIR}/"
+popd
+
+mkdir -p "${TOOLS_DIR}/DEBIAN"
+cat > "${TOOLS_DIR}/DEBIAN/control" <<EOF
+Package: linux-tools-${kuname}
+Version: ${kpkg_version}
+Architecture: ${ARCH}
+Maintainer: Havock <systems-engineering@coreweave.com>
+Provides: linux-tools-generic
+Depends: libtraceevent1 (>=1:1.3.3-1), libllvm14 (>= 1:14.0.0-1ubuntu1.1)
+Description: A minimial debian package that delivers
+  the specific binaries of the linux-tools package
+  that Coreweave wants to distribute.
+EOF
+
+cat > ${TOOLS_DIR}/DEBIAN/postinst <<EOF
+#!/bin/sh
+
+path="/usr/lib/linux-tools-cw/$kuname"
+install_path="/usr/bin"
+
+if [ -f \$install_path/perf ] || [ -f \$install_path/bpftool ]; then
+    rm -rf \$install_path/perf
+    rm -rf \$install_path/bpftool
+fi
+
+ln -s \$path/perf \$install_path/perf
+ln -s \$path/bpftool \$install_path/bpftool
+EOF
+
+chmod 0755 ${TOOLS_DIR}/DEBIAN/postinst
+dpkg-deb --root-owner-group --build ${TOOLS_DIR}/
 
 # Move build artifacts
-echo -e "\n=== Step 7: Moving build artifacts ==="
+echo -e "\n=== Step 8: Moving build artifacts ==="
 echo "Moving build artifacts to output directory..."
-ls -al "${BUILD_DIR}"
-mv -v "${BUILD_DIR}"/*.deb /output/
-mv -v "${BUILD_DIR}"/*.buildinfo /output/
-mv -v "${BUILD_DIR}"/*.changes /output/
+ls -al "${BUILD_DIR}/${SRC_PARENT_DIR}"
+mv -v "${BUILD_DIR}/${SRC_PARENT_DIR}"/*.deb /output/
+mv -v "${BUILD_DIR}/${SRC_PARENT_DIR}"/*.buildinfo /output/
+mv -v "${BUILD_DIR}/${SRC_PARENT_DIR}"/*.changes /output/
 
 echo -e "\n=== Build complete ==="
 echo "Output files are in the output directory."
